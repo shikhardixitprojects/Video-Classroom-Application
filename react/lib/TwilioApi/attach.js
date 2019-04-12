eb1f5d25e835ad023e364ea640009b03dac104e7
Shikhar <ShikharDixit@Shikis-MacBook-Air.local>
2019-04-11 21:27:12 -0400

    created handler for students coming earlier than prof

--- react/lib/TwilioApi/attach.js
+++ react/lib/TwilioApi/attach.js
@@ -7,51 +7,25 @@ const attach = (twilioConnection, videoElement, roomName) => {
         instructor = value
       }
     }
-
-    // BUG: right now instructor needs to be connected before all students
-    // add a listener to the participantConnected event to account for this
-    if(!instructor) throw new Error("No Instructor")
-
-    instructor.on("trackStarted", (track) => {
-      track.attach(videoElement)
-      console.log(track)
-    })
+    if(instructor) {
+      instructor.on("trackStarted", (track) => {
+        track.attach(videoElement)
+        console.log(track)
+      })
+    }
+      room.on('participantConnected', participant => {
+        if(instructor !== undefined){
+          return;
+        }
+        if(participant.tracks.size > 0){
+          instructor = participant
+        }
+        instructor.on("trackStarted", (track) => {
+          track.attach(videoElement)
+        })
+      });
   })
 }
 
 export const attachWebcam = (twilioConnection, videoElement) => attach(twilioConnection, videoElement, "videostream")
 export const attachDesktop = (twilioConnection, videoElement) => attach(twilioConnection, videoElement, "screenshare")
-
-// import twilioConnect from './twilioConnection'
-//
-// export default function(videoElement) {
-//
-//   twilioConnect("videostream").then(room => {
-//     console.log(`Successfully joined a Room: ${room}`);
-//
-//
-//     room.on('participantConnected', participant => {
-//
-//       console.log(`A remote Participant connected: ${participant}`);
-//       console.log(videoElement)
-//
-//       participant.tracks.forEach(publication => {
-//
-//         console.log(publication, publication.isSubscribed)
-//
-//         if (publication.isSubscribed) {
-//           const track = publication.track;
-//           track.attach(videoElement);
-//       }
-//       });
-//
-//     });
-//
-//
-//
-//   }, error => {
-//     console.error(`Unable to connect to Room: ${error.message}`);
-//   });
-//
-//   return {}
-// }
